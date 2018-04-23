7b6e3ea69d77e669e343f6b4a5636d3692a014a0
Mike Busho <bushomichael@gmail.com>
2018-04-23 23:29:21 +0300

    Initial commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Rank =() => {
+	return (
+		<div >	
+			<div className='white f3'> 
+				{'Mike Bsh'}	
+			</div>
+			<div className='white f1'> 
+				{'Test'}	
+			</div>
+		</div>
+		);
+}
+
+export default Rank;
